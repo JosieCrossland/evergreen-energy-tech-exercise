fcef1000a3a6faa05ae032f5a1e029faf343e86e
JosieCrossland <josiecrossland123@gmail.com>
2021-11-12 11:39:06 +0000

    tech exercise - added calc for total costs and some tidying up

--- quote_utils.py
+++ quote_utils.py
@@ -1,12 +1,17 @@
-def calculate_heat_loss(floor_area, heating_factor, insulation_factor):
+from typing import List
+
+
+def calculate_heat_loss(
+    floor_area: int, heating_factor: int, insulation_factor: float
+) -> float:
     return floor_area * heating_factor * insulation_factor
 
 
-def calculate_power_heat_loss(heat_loss, heating_degree_days):
+def calculate_power_heat_loss(heat_loss: float, heating_degree_days: int) -> float:
     return round((heat_loss / heating_degree_days), 1)
 
 
-def select_heat_pump(heat_pump_data, required_capacity):
+def select_heat_pump(heat_pump_data: List[dict], required_capacity: float) -> dict:
     highest_capacity_pump = max(heat_pump_data, key=lambda item: item["outputCapacity"])
     sorted_pumps = sorted(heat_pump_data, key=lambda item: item["outputCapacity"])
 
@@ -16,3 +21,16 @@ def select_heat_pump(heat_pump_data, required_capacity):
     for heat_pump in sorted_pumps:
         if required_capacity <= heat_pump.get("outputCapacity"):
             return heat_pump
+
+
+def calculate_total_installation_cost(heat_pump: dict, vat_rate: int) -> float:
+    costs = heat_pump.get("costs")
+    net_total = 0
+    for cost_item in costs:
+        for key in cost_item.keys():
+            if key == "cost":
+                net_total += cost_item.get(key)
+
+    vat = (net_total / 100) * vat_rate
+
+    return round(net_total + vat, 2)
--- test_quote_utils.py
+++ test_quote_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized
-from quote_utils import calculate_heat_loss, calculate_power_heat_loss, select_heat_pump
+import quote_utils as sut
 
 
 class TestQuoteUtils(unittest.TestCase):
@@ -10,29 +10,29 @@ class TestQuoteUtils(unittest.TestCase):
                 "label": "5kW package",
                 "outputCapacity": 5,
                 "costs": [
-                    {"label":  "Design", "cost":  2000},
-                    {"label":  "Installation", "cost":  1500},
-                    {"label":  "Cups of tea", "cost":  20}
-                ]
+                    {"label": "Design", "cost": 2000},
+                    {"label": "Installation", "cost": 1500},
+                    {"label": "Cups of tea", "cost": 20},
+                ],
             },
             {
                 "label": "2kW package",
                 "outputCapacity": 2,
                 "costs": [
-                    {"label":  "Design", "cost":  4000},
-                    {"label":  "Installation", "cost":  1500},
-                    {"label":  "Cups of tea", "cost":  40}
-                ]
+                    {"label": "Design", "cost": 4000},
+                    {"label": "Installation", "cost": 1500},
+                    {"label": "Cups of tea", "cost": 40},
+                ],
             },
             {
                 "label": "10kW package",
                 "outputCapacity": 10,
                 "costs": [
-                    {"label":  "Design", "cost":  6000},
-                    {"label":  "Installation", "cost":  1500},
-                    {"label":  "Cups of tea", "cost":  900}
-                ]
-            }
+                    {"label": "Design", "cost": 6000},
+                    {"label": "Installation", "cost": 1500},
+                    {"label": "Cups of tea", "cost": 900},
+                ],
+            },
         ]
 
     def test_calculate_heat_loss(self):
@@ -40,26 +40,41 @@ class TestQuoteUtils(unittest.TestCase):
         heating_factor = 101
         insulation_factor = 1.3
         expected = 16412.5
-        actual = calculate_heat_loss(floor_area, heating_factor, insulation_factor)
+        actual = sut.calculate_heat_loss(floor_area, heating_factor, insulation_factor)
         self.assertEqual(expected, actual)
 
     def test_calculate_power_heat_loss(self):
         heat_loss = 16412.5
         heating_degree_days = 2483
         expected = 6.6
-        actual = calculate_power_heat_loss(heat_loss, heating_degree_days)
+        actual = sut.calculate_power_heat_loss(heat_loss, heating_degree_days)
         self.assertEqual(expected, actual)
 
-    @parameterized.expand([
-        (2, "2kW package"),
-        (2.1, "5kW package"),
-        (5.1, "10kW package"),
-        (11, "required capacity exceeds current heat pump range"),
-    ])
+    @parameterized.expand(
+        [
+            (2, "2kW package"),
+            (2.1, "5kW package"),
+            (5.1, "10kW package"),
+            (11, "required capacity exceeds current heat pump range"),
+        ]
+    )
     def test_select_heat_pump(self, required_capacity, expected):
-        actual = select_heat_pump(self.heat_pump_test_data, required_capacity)
+        actual = sut.select_heat_pump(self.heat_pump_test_data, required_capacity)
         self.assertEqual(expected, actual.get("label"))
 
+    @parameterized.expand(
+        [
+            (0, 3696.00),
+            (1, 5817.00),
+            (2, 8820.00),
+        ]
+    )
+    def test_calculate_total_installation_cost(self, pump_index, expected):
+        selected_heat_pump = self.heat_pump_test_data[pump_index]
+        vat_rate = 5
+        actual = sut.calculate_total_installation_cost(selected_heat_pump, vat_rate)
+        self.assertEqual(expected, actual)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
